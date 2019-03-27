6a72d1d868bc735a5f363ce6db6ba3ae3f2357a6
Victor Despland <algruffel@tsf-484-wpa-4-206.epfl.ch>
2019-03-27 15:38:25 +0100

    h

--- EX7/Analyse.m
+++ EX7/Analyse.m
@@ -1,6 +1,6 @@
 %% Chargement des resultats %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-fichier = '3';
+fichier = 'output';
 data = load([fichier,'_u.out']);
 x = data(:,1);
 u = data(:,2);
@@ -50,47 +50,5 @@ ylim([min(f(:)),max(f(:))])
 %     set(ht,'String',sprintf('t=%0.2f s',t(i)))
 % end
 
-    switch(cb_gauche)
-    {
-      case fixe:
-        fnext[0] = fnow[0]; // TODO : Completer la condition au bord gauche fixe
-        break;
-
-      case libre:
-        fnext[0] = fnext[1]; // TODO : Completer la condition au bord gauche libre
-        break;
-
-      case harmonique:
-        fnext[0] = A*sin(omega*t); // TODO : Completer la condition au bord gauche harmonique
-        break;
-
-      case sortie:
-        fnext[0] = fnow[0]+sqrt((*u2)(0.5*dx))*dt/dx*(fnow[1]-fnow[0]); // TODO : Completer la condition au bord gauche "sortie de l'onde"
-        break;
-    }
-
-    switch(cb_droit)
-    {
-      case fixe:
-      fnext[N-1] = fnow[N-1]; // TODO : Completer la condition au bord droit fixe
-        break;
-
-      case libre:
-        fnext[N-1] = fnext[N-2]; // TODO : Completer la condition au bord droit libre
-        break;
-
-      case harmonique:
-        fnext[N-1] = A*sin(omega*t); // TODO : Completer la condition au bord droit harmonique
-        break;
-
-      case sortie:
-        fnext[N-1] = fnow[0]-sqrt((*u2)((N-0.5)*dx))*dt/dx*(fnow[N-1]-fnow[N-2]);; // TODO : Completer la condition au bord droit "sortie de l'onde"
-        break;
-    }
     
-    
-    double E(0.0);
-  for(size_t i(0); i<f.size()-1; i++){
-    E+=(f[i]+f[i+1])*0.5*dx;
-  }
-  return E;
+   
