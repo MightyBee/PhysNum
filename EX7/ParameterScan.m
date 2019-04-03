28a2d8359046fa260936d15dd82424af639a9dd4
Victor Despland <algruffel@tsf-484-wpa-4-117.epfl.ch>
2019-04-03 15:09:59 +0200

    fff

--- EX7/ParameterScan.m
+++ EX7/ParameterScan.m
@@ -20,13 +20,14 @@ nsimul = 10; % Nombre de simulations a faire
 
 % N = round((logspace(1,3, nsimul)./4))*4+1;
 N=round(logspace(2,3, nsimul));
-
-paramstr = 'Npoints'; % Nom du parametre a scanner
-param = N; % Valeurs du parametre a scanner
+w=3.1416*u/L*[1,2,3,4,5,6,7,8,9,10];
+paramstr = 'omega'; % Nom du parametre a scanner
+param = w; % Valeurs du parametre a scanner
 
 L=20;
 dx=L./(N-1);
 x0=5;
+u=6;
 
 %% Simulations %%
 %%%%%%%%%%%%%%%%%
@@ -62,7 +63,18 @@ for i = 1:nsimul % Parcours des resultats de toutes les simulations
 end
 
 % [a,erra,yFit]=fit(dt',Tp');
-
+if(strcmp(paramstr,'omega'))
+      y = zeros(1,nsimul);
+    for i=1:nsimul
+        data =  load([output{i} '_E.out']);
+        E = data(:,2:end);
+        for j=1:size(E,1)
+            if E(i,2)>y(i)
+                y(i)=E(i,2)
+            end
+        end
+    end
+end
 
 %% Figures %%
 %%%%%%%%%%%%%
@@ -78,4 +90,10 @@ if(strcmp(paramstr,'Npoints'))
     lgd=legend('Valeurs numériques', 'Régression linéaire');
     set(lgd,'fontsize',14,'Location','northwest');
 %     print(fig1,'conv', '-depsc');
+end
+
+if(strcmp(paramstr,'omega'))
+    figure
+    h=plot([1 10],y,'k+');
+    grid on
 end
